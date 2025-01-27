4018bea778c3f777b6cd9c50e4e63ee654608078
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-01-27 16:18:25 +0530

    Update SingleNumber.cpp

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
         for(int val:nums){
-            ans = ans^val;
+            ans = ans^val; //using XOR method
         } return ans;
     }
 };
