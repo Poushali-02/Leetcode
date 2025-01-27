4d43b3a282931b8916471233093d59b4434deef6
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-01-27 16:40:11 +0530

    Create Majority.cpp
    Solved Leetcode problem 169

---
+++ Majority.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {//Moore's algorithm
+        int f = 0, ans = 0; 
+        for(int i=0; i<nums.size(); i++){
+            if(f==0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]) 
+            f++;
+            else f--;
+        }
+        return ans;
+    }
+};
