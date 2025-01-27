3bc372d3752cb8f634aa38bf36a6ff15435072e1
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-01-27 16:16:54 +0530

    Update MaxSubArraySum.cpp

--- MaxSubArraySum.cpp
+++ MaxSubArraySum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-    int maxSum = INT_MIN;
+    int maxSum = INT_MIN; //Kadane's Algorithm
     int currSum = 0;
     for(int st = 0; st < nums.size(); st++){
             currSum += nums.at(st);
             maxSum = max(currSum, maxSum);
-            if(currSum < 0) currSum = 0;
+            if(currSum < 0) currSum = 0; //reset for negative value
     } return maxSum;
     }
 };
