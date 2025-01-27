f4da7bc24ad54eacb0e5221f9f8093af8b082d99
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-01-27 16:00:34 +0530

    Add files via upload

---
+++ Pow(x,n).cpp
@@ -0,0 +1,18 @@
+class Solution { 
+public:
+    double myPow(double x, long long int n) {
+        double ans=1; //creating the answer variable
+        if(n<0){
+            x = 1/x; //formatting for negative n
+        }
+        long long int bin = abs(n);
+        while(bin>0){
+            if(bin % 2 == 1){ //accessing binary
+                ans *= x; //updating answer
+                }
+            bin /= 2;
+            x = x*x; //updating x
+        }
+        return ans;
+    }
+};
